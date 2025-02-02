ba40ad188ae4a7b31f2944f72367558516057b1a
Ivy <nguyeniv@msu.edu>
2025-02-01 21:36:15 -0500

    home page

--- my-vue-app/src/pages/home.jsx
+++ my-vue-app/src/pages/home.jsx
@@ -1,13 +1,34 @@
 import React from "react";
-import {getUserInfo} from "/src/script.jsx"
+import { useState, useEffect } from "react";
+import { getProfileImage } from "/src/script.jsx";
+
+import "./home.css"
 
 function Home() {
-    getUserInfo()
-    return(
+    const handleLogin = () => {
+          getProfileImage();
+        };
+
+    return (
     <main>
-        <div>
-            <p>test :)</p>
-        </div>
+      <head>
+        <link rel="stylesheet" href="/home.css"></link>
+      </head>
+      <body>
+      <h1>Display your Spotify profile data</h1>
+      
+        <section id="profile">
+        <h2>Logged in as <span id="displayName"></span></h2>
+        <span id="avatar"></span>
+        <ul>
+            <li>User ID: <span id="id"></span></li>
+            <li>Email: <span id="email"></span></li>
+            <li>Spotify URI: <a id="uri" href="#"></a></li>
+            <li>Link: <a id="url" href="#"></a></li>
+            <li>Profile Image: <img src={getProfileImage}/></li>
+        </ul>
+        </section>
+      </body>
     </main>
     );
 };
