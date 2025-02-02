9c8c5bcde7d78bfd097798e78da56a769923d0f0
Apar Mohabansi <apar.mohabansi@gmail.com>
2025-02-01 21:45:08 -0500

    Added my pic to home :)

--- my-vue-app/src/script.jsx
+++ my-vue-app/src/script.jsx
@@ -2,15 +2,13 @@ const clientId = "cef9cca2812d435dac59804107b8fcba"; // Replace with your client
 const params = new URLSearchParams(window.location.search);
 const code = params.get("code");
 
-// if (!code) {
-//     redirectToAuthCodeFlow(clientId);
-// } else {
-//     const accessToken = await getAccessToken(clientId, code);
-//     const profile = await fetchProfile(accessToken);
-//     console.log(profile); // Profile data logs to console
+if(code){
+    const accessToken = await getAccessToken(clientId, code);
+    const profile = await fetchProfile(accessToken);
+    console.log(profile); // Profile data logs to console
 
-//     populateUI(profile);
-// }
+    populateUI(profile);
+}
 
 
 export async function redirectToAuthCodeFlow(clientId) {
@@ -106,21 +104,20 @@ export async function getUserInfo(){
 }
 
 function populateUI(profile) {
-    // document.getElementById("displayName").innerText = profile.display_name;
-    // if (profile.images[0]) {
-    //     const profileImage = new Image(200, 200);
-    //     profileImage.src = profile.images[0].url;
-    //     document.getElementById("avatar").appendChild(profileImage);
-    //     document.getElementById("imgUrl").innerText = profile.images[0].url;
-    // }
+    document.getElementById("displayName").innerText = profile.display_name;
+    if (profile.images[0]) {
+        const profileImage = new Image(200, 200);
+        profileImage.src = profile.images[0].url;
+        document.getElementById("avatar").appendChild(profileImage);
+        document.getElementById("imgUrl").innerText = profile.images[0].url;
+    }
 
-    sendProfileIdToBackend(profile.id);
-    // document.getElementById("id").innerText = profile.id;
-    // document.getElementById("email").innerText = profile.email;
-    // document.getElementById("uri").innerText = profile.uri;
-    // document.getElementById("uri").setAttribute("href", profile.external_urls.spotify);
-    // document.getElementById("url").innerText = profile.href;
-    // document.getElementById("url").setAttribute("href", profile.href);
+    document.getElementById("id").innerText = profile.id;
+    document.getElementById("email").innerText = profile.email;
+    document.getElementById("uri").innerText = profile.uri;
+    document.getElementById("uri").setAttribute("href", profile.external_urls.spotify);
+    document.getElementById("url").innerText = profile.href;
+    document.getElementById("url").setAttribute("href", profile.href);
 }
 
 export async function fetchRecentlyPlayedSongs() {
