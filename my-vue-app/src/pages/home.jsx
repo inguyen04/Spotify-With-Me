e031fad98bbc1879c43b817366045ec3f212cad8
Ivy <nguyeniv@msu.edu>
2025-02-01 22:40:02 -0500

    homepage

--- my-vue-app/src/pages/home.jsx
+++ my-vue-app/src/pages/home.jsx
@@ -1,36 +1,31 @@
 import React from "react";
-import { useState, useEffect } from "react";
-import { getProfileImage } from "/src/script.jsx";
-
-import "./home.css"
+import "./home.css";
 
 function Home() {
-    const handleLogin = () => {
-          getProfileImage();
-        };
 
     return (
-    <main>
-      <head>
-        <link rel="stylesheet" href="/home.css"></link>
-      </head>
-      <body>
-      <h1>Display your Spotify profile data</h1>
-      
-        <section id="profile">
-        <h2>Logged in as <span id="displayName"></span></h2>
-        <span id="avatar"></span>
-        <ul>
-            <li>User ID: <span id="id"></span></li>
-            <li>Email: <span id="email"></span></li>
-            <li>Spotify URI: <a id="uri" href="#"></a></li>
-            <li>Link: <a id="url" href="#"></a></li>
-            <li>Profile Image: <img src={getProfileImage}/></li>
-        </ul>
-        </section>
-      </body>
-    </main>
+        <main>
+            <h1>Display your Spotify profile data</h1>
+            <div className="home-page">
+                <div className="left-col">
+                    <h2>Feed!!!!</h2>
+                    <p>This is the left column content.</p>
+                </div>
+                <div className="right-col">
+                    <h2>Right Column</h2>
+                    <h2>Logged in as <span id="displayName"></span></h2>
+                    <span id="avatar"></span>
+                    <ul>
+                        <li>User ID: <span id="id"></span></li>
+                        <li>Email: <span id="email"></span></li>
+                        <li>Spotify URI: <a id="uri" href="#"></a></li>
+                        <li>Link: <a id="url" href="#"></a></li>
+                        <li>Profile Image: <span id="imgUrl"></span></li>
+                    </ul>
+                </div>
+            </div>
+        </main>
     );
-};
+}
 
 export default Home;
