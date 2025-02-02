8c1e5395bb5cd8060b3b398bf4688e4f28772e38
Erik <erikwblake@gmail.com>
2025-02-01 19:22:16 -0500

    Established route for blueprints to follow and reorganized app.py and routes.py

--- server/routes/routes.py
+++ server/routes/routes.py
@@ -1,21 +1,21 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask, request, jsonify
+from flask import Blueprint, request, jsonify
 from supabase import create_client, Client
 from flask_cors import CORS
 
-app = Flask(__name__)
-CORS(app)
+
 load_dotenv()
 
+routes = Blueprint("routes", __name__)
 
 
 SUPABASE_URL = os.getenv('SUPABASE_URL')
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
-supabase: Client = create_client(os.getenv(SUPABASE_URL), SUPABASE_KEY)
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
-@app.route('/receive-data', methods=['POST'])
+@routes.route('/receive-data', methods=['POST'])
 def receive_data():
   try:
     data = request.get_json()
@@ -26,12 +26,13 @@ def receive_data():
     response = supabase.table("users").insert({"user_id": user_id}).execute()
 
     return jsonify({"message": "User added successfuly", "response": response}), 201
+  
   except Exception as e:
     print("Error:", str(e))
     return jsonify({"error": str(e)}), 500
 
 
-@app.route('/add-review')
+@routes.route('/add-review', methods=['POST'])
 def add_review():
   try:
     data = request.get_json()
@@ -42,29 +43,22 @@ def add_review():
     if not user_id or (rating is None and review is None):
       return jsonify({"error": "Missing required fields"}), 400
     
-
     response = supabase.table("reviews").insert({
       "user_id": user_id,
       "rating": rating,
       "review": review
     }).execute()
 
-    return jsonify({"message": "Review added successfully"}, "response": response)
+    return jsonify({"message": "Review added successfully", "response": response}),
   
   except Exception as e:
     print("Error:" str(e))
     return jsonify({"error": str(e)}), 500
   
-@app.route('/get-reviews/<user_id>', methods=['GET'])
+@routes.route('/get-reviews/<user_id>', methods=['GET'])
 def get_review(user_id):
   try:
     response = supabase.table("reviews").select("*").eq("user_id", user_id).execute()
     return jsonify({"reviews": response}), 200
   except Exception as e:
     return jsonify({"error": str(e)}), 500
-
-
-
-
-if __name__ == '__main__':
-  app.run(debug=True)
